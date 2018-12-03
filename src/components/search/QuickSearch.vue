a2089461dc236307dfe2b923411233797a57ac21
gstephe7 <gstephe7@gmail.com>
2018-12-02 23:15:49 -0500

    style quick search

--- src/components/search/QuickSearch.vue
+++ src/components/search/QuickSearch.vue
@@ -3,7 +3,7 @@
     <div class="layer">
       <h2>Search For Steel Near You</h2>
       <div class="search">
-        <div>
+        <div class="search-section">
           <select class="input">
             <option disabled selected>Shape</option>
           </select>
@@ -11,7 +11,7 @@
             <option disabled selected>Dimension</option>
           </select>
         </div>
-        <div>
+        <div class="search-section">
           <input class="input" type="text" placeholder="Length"/>
           <input class="input" type="text" placeholder="Zipcode">
         </div>
@@ -48,22 +48,42 @@ export default {
   h2 {
     color: $secondary;
     text-align: center;
+    font-weight: 300;
+    margin-top: 0;
   }
 
   .search {
     display: flex;
     flex-direction: column;
+    align-items: center;
+  }
+
+  .search-section {
+    display: flex;
+    justify-content: space-around;
   }
 
   .input {
     width: 140px;
     padding: 10px;
+    margin: 5px;
     border-radius: 5px;
     border: none;
     font-size: 18px;
+    font-family: 'Roboto', sans-serif;
+    font-weight: 300;
   }
 
   .submit {
     width: 200px;
+    margin: 10px;
+    padding: 5px;
+    background-color: $secondary;
+    border-radius: 10px;
+    border: none;
+    color: #fff;
+    font-family: 'Roboto', sans-serif;
+    font-size: 18px;
+    cursor: pointer;
   }
 </style>
