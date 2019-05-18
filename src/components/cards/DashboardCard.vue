f1925ed997d6959cf8c8dd78647666cfa7b04873
Greg <gstephe7@gmail.com>
2019-05-18 07:40:08 -0400

    dashboard card

---
+++ src/components/cards/DashboardCard.vue
@@ -0,0 +1,28 @@
+<template>
+  <div class="dashboard-card">
+
+    <!-- Thumbnail -->
+    <div class="thumbnail">
+      <slot name="thumbnail"></slot>
+    </div>
+
+    <!-- Title -->
+    <div class="title">
+      <slot name="title"></slot>
+    </div>
+
+  </div>
+</template>
+
+<script>
+export default {
+}
+</script>
+
+<style lang="scss" scoped>
+  @import '@/assets/scss/variables.scss';
+
+  .dashboard-card {
+
+  }
+</style>
